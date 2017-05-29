3c31f981c6ecf90f6829de8a8ce86acddc1770ac
zaleslaw <zaleslaw.sin@gmail.com>
2017-05-29 18:38:42 +0300

    Added functional programming module

---
+++ src/main/java/Chapter_7_Functional_Programming/Ex_1_Composition_in_Java.java
@@ -0,0 +1,38 @@
+package Chapter_7_Functional_Programming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * Overview of function composition in Java 8
+ */
+public class Ex_1_Composition_in_Java {
+    public static void main(String[] args) {
+        List<Integer> fibonacciNumbers = Arrays.asList(1,1,2,3,5,8,13,21);
+
+        // Step 1
+        fibonacciNumbers.stream().map(e -> e + 1).forEach(System.out::println);
+
+        // Step 2: Extract the function
+        final Function<Integer, Integer> addOne = e -> e + 1;
+        fibonacciNumbers.stream().map(addOne).forEach(System.out::println);
+
+        // Step 3: Pipe two functions in direct order
+        Function<Integer, Integer> multiplyTen = e -> e * 10;
+        fibonacciNumbers.stream().map(addOne.andThen(multiplyTen)).forEach(System.out::println);
+
+
+        // Step 4: Pipe two functions in opposite order
+        fibonacciNumbers.stream().map(addOne.compose(e-> e*10)).forEach(System.out::println);
+
+        // Step 5: Use custom compose
+        fibonacciNumbers.stream().map(compose(addOne, multiplyTen)).forEach(System.out::println);
+
+
+    }
+
+    public static <Integer> Function<Integer, Integer> compose(Function<Integer,Integer> f, Function<Integer,Integer> g) {
+        return x -> f.apply(g.apply(x));
+    }
+}
---
+++ src/main/java/Chapter_7_Functional_Programming/Ex_3_Custom_Composition_in_Java.java
@@ -0,0 +1,14 @@
+package Chapter_7_Functional_Programming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * Make composition for two-argument functions
+ */
+public class Ex_3_Custom_Composition_in_Java {
+    public static void main(String[] args) {
+        //https://dzone.com/articles/higher-order-functions
+    }
+}
