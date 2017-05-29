c26a9ebe33227dc4c14ad4bbb32333ba104e8cc7
zaleslaw <zaleslaw.sin@gmail.com>
2017-05-29 20:30:44 +0300

    Added Collectors module

---
+++ src/main/java/Chapter_5_Collectors/Ex_4_Joining.java
@@ -0,0 +1,32 @@
+package Chapter_5_Collectors;
+
+import Chapter_3_Migration_from_7_to_8.YoungEnterpriseCode;
+import Chapter_3_Migration_from_7_to_8.beans.Employee;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Alexey_Zinovyev on 29-May-17.
+ */
+public class Ex_4_Joining {
+    public static void main(String[] args) {
+
+        final List<Employee> employees = YoungEnterpriseCode.hireFourEmployees();
+        String names = employees.stream()
+                .map(Employee::getName)
+                .collect(Collectors.joining());
+
+        String  delimitedNames = employees.stream()
+                .map(Employee::getName)
+                .collect(Collectors.joining("# "));
+
+        String  prefixedNames = employees.stream()
+                .map(Employee::getName)
+                .collect(Collectors.joining(" $ ", "Our interns: ",  ". Pay them!"));
+
+        System.out.println("Joined names:  "  + names);
+        System.out.println("Joined,  delimited names:  "  + delimitedNames);
+        System.out.println(prefixedNames);
+    }
+}
